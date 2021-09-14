cf13c3ed1a3d8775509e0339fcc782ee60bc963b
aiasci <42707558+aiasci@users.noreply.github.com>
2021-09-14 06:55:12 +0300

    commit
    commit

--- graphs.R
+++ graphs.R
@@ -5,8 +5,83 @@ library(readxl)
 library(plotly)
 
 
-a<- data.table(read_xlsx("/home/ismetovic/Documents/GitHub/firms/other_var.xlsx"))
+a<- data.table(read.csv("https://raw.githubusercontent.com/aiasci/firms/main/other_var.csv"))
+a<- a[,c(2:5)]
 colnames(a)[1] <- "year"
+
+firmtype = c()
+for (i in c(1:16)){
+  firmtype[i] = (i-1)*4+1
+}
+
+firmname = function(x){
+  if (x==1) 
+    paste0("A-Agriculture, forestry and fishing")
+  else if (x==5)
+    paste0("B-Mining and quarrying")
+  else if (x==9)
+    paste0("C-Manufacturing")
+  else if (x==13)
+    paste0("D-Electricity, gas, steam and air conditioning supply")
+  else if (x==17)
+    paste0("E-Water supply; sewerage; waste management and remediation activities")
+  else if (x==21)
+    paste0("F-Construction")
+  else if (x==25)
+    paste0("G-Wholesale and retail trade; repair of motor vehicles and motorcycles")
+  else if (x==29)
+    paste0("H-Transporting and storage")
+  else if (x==33)
+    paste0("I-Accommodation and food service activities")
+  else if (x==37)
+    paste0("J-Information and communication")
+  else if (x==41)
+    paste0("L-Real estate activities")
+  else if (x==45)
+    paste0("M-Professional, scientific and technical activities")
+  else if (x==49)
+    paste0("N-Administrative and support service activities")
+  else if (x==53)
+    paste0("P-Education")
+  else if (x==57)
+    paste0("Q-Human health and social work activities")
+  else if (x==61)
+    paste0("R-Arts, entertainment and recreation")
+}
+firmsign = function(x){
+  if (x==1) 
+    paste0("a")
+  else if (x==5)
+    paste0("b")
+  else if (x==9)
+    paste0("c")
+  else if (x==13)
+    paste0("d")
+  else if (x==17)
+    paste0("e")
+  else if (x==21)
+    paste0("f")
+  else if (x==25)
+    paste0("g")
+  else if (x==29)
+    paste0("h")
+  else if (x==33)
+    paste0("i")
+  else if (x==37)
+    paste0("j")
+  else if (x==41)
+    paste0("l")
+  else if (x==45)
+    paste0("m")
+  else if (x==49)
+    paste0("n")
+  else if (x==53)
+    paste0("p")
+  else if (x==57)
+    paste0("q")
+  else if (x==61)
+    paste0("r")
+}
 
 graphtype = function(x){
   if (x == unique(a$code)[1])
@@ -69,37 +144,54 @@ graphtype = function(x){
     paste0("equ")}
 
 for (i in c(1:length(unique(a$code)))) {
+  for (j in firmtype){
+    c= data.table()
+    c$year = a[a$name ==   unique(a$name)[j]][a[a$name == unique(a$name)[j]]$code == unique(a$code)[i]]$year
+    c$Large = a[a$name ==  unique(a$name)[j]][a[a$name == unique(a$name)[j]]$code == unique(a$code)[i]]$data
+    c$Medium = a[a$name == unique(a$name)[j+1]][a[a$name == unique(a$name)[j+1]]$code == unique(a$code)[i]]$data
+    c$Micro = a[a$name ==  unique(a$name)[j+2]][a[a$name == unique(a$name)[j+2]]$code == unique(a$code)[i]]$data
+    c$Small = a[a$name ==  unique(a$name)[j+3]][a[a$name == unique(a$name)[j+3]]$code == unique(a$code)[i]]$data
+    assign(paste0(firmsign(j),"_",graphtype(unique(a$code)[i])),
+           plot_ly(data=c,type = 'scatter', mod= 'line',mode = 'markers')%>%
+             add_trace(x = ~year,y= ~Large, name = "Large-Sized", mode = "lines+markers", color = "#b2abd2",visible = T)%>%
+             add_trace(x = ~year,y= ~Medium, name = "Medium-Sized", mode = "lines+markers", color = "#b2abd1",visible = T)%>%  
+             add_trace(x = ~year,y= ~Small, name = "Small-Sized", mode = "lines+markers", color = "#b2abd0",visible = T)%>%
+             add_trace(x = ~year,y= ~Micro, name = "Micro-Sized", mode = "lines+markers", color = "#b2abd3",visible = T)%>%
+             layout(title = list(text = paste0(unique(a$code)[i],
+                                               '<br>',
+                                               '<sup>',
+                                               firmname(j),
+                                               '</sup>')),
+                    xaxis=list(title="Year", tickvals= list(2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021)),
+                    yaxis=list(title="Percentage"),
+                    annotations = list(x = 0, y = -0.07, text = "Source:CBRT,Author's Calculations", 
+                                       showarrow = F, xref='paper', yref='paper', 
+                                       xshift=0, yshift=0,
+                                       font=list(size=15, color="black"))), 
+           envir = .GlobalEnv)
+    
+  }
   c= data.table()
-  c$year = a[a$name ==   unique(a$name)[1]][a[a$name == unique(a$name)[1]]$code == unique(a$code)[i]]$year
-  c$Large = a[a$name ==  unique(a$name)[1]][a[a$name == unique(a$name)[1]]$code == unique(a$code)[i]]$data
-  c$Medium = a[a$name == unique(a$name)[2]][a[a$name == unique(a$name)[2]]$code == unique(a$code)[i]]$data
-  c$Micro = a[a$name ==  unique(a$name)[3]][a[a$name == unique(a$name)[3]]$code == unique(a$code)[i]]$data
-  c$Small = a[a$name ==  unique(a$name)[4]][a[a$name == unique(a$name)[4]]$code == unique(a$code)[i]]$data
-  assign(paste0("a_",graphtype(unique(a$code)[i])),
+  c$year = a[a$name ==   unique(a$name)[65]][a[a$name == unique(a$name)[65]]$code == unique(a$code)[i]]$year
+  c$Medium = a[a$name == unique(a$name)[65]][a[a$name == unique(a$name)[65]]$code == unique(a$code)[i]]$data
+  c$Micro = a[a$name ==  unique(a$name)[66]][a[a$name == unique(a$name)[66]]$code == unique(a$code)[i]]$data
+  c$Small = a[a$name ==  unique(a$name)[67]][a[a$name == unique(a$name)[67]]$code == unique(a$code)[i]]$data
+  assign(paste0("s","_",graphtype(unique(a$code)[i])),
          plot_ly(data=c,type = 'scatter', mod= 'line',mode = 'markers')%>%
-           add_trace(x = ~year,y= ~Large, name = "Large-Sized", mode = "lines+markers", color = "#b2abd2",visible = T)%>%
            add_trace(x = ~year,y= ~Medium, name = "Medium-Sized", mode = "lines+markers", color = "#b2abd1",visible = T)%>%  
            add_trace(x = ~year,y= ~Small, name = "Small-Sized", mode = "lines+markers", color = "#b2abd0",visible = T)%>%
            add_trace(x = ~year,y= ~Micro, name = "Micro-Sized", mode = "lines+markers", color = "#b2abd3",visible = T)%>%
            layout(title = list(text = paste0(unique(a$code)[i],
                                              '<br>',
                                              '<sup>',
-                                             "A-Agriculture, forestry and fishing",
+                                             "S-Other services activities",
                                              '</sup>')),
                   xaxis=list(title="Year", tickvals= list(2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021)),
                   yaxis=list(title="Percentage"),
-                  annotations = list(x = 0, y = -0.07, text = "Source:CBRT, Autors Calculations", 
+                  annotations = list(x = 0, y = -0.07, text = "Source:CBRT,Author's Calculations", 
                                      showarrow = F, xref='paper', yref='paper', 
                                      xshift=0, yshift=0,
                                      font=list(size=15, color="black"))), 
          envir = .GlobalEnv)
-  
-}
-
-
-
-
-
-
-
+}  
 
